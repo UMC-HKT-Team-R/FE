59db9253304c35ae0491b4c3338a0b4b6081a837
jisooooooooooo <dlawltn0708@naver.com>
2025-01-11 22:53:36 +0900

    [fix] 헤더 수정

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import logo from "../../assets/logo.svg";
 
 function Header() {
   return (
-    <header className="py-[22px] px-[16px] fixed top-0 left-0 w-full bg-white z-10">
+    <header className="py-[22px] max-w-[468px] px-[16px] fixed top-0 left-0 w-full bg-white z-10 -translate-x-1/2 left-1/2">
       <img src={logo} alt="로고" className="w-[131.2px] h-[24px]" />
     </header>
   );
