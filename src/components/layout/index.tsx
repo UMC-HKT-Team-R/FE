e2bf22477851f04acd7c14f26235ff87ac7eb8e4
Sohyunnnn <thgus05061@naver.com>
2025-01-11 18:11:22 +0900

    style: 레이아웃 설정

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="font-pretendard">
+    <div className="flex font-pretendard mx-auto h-full min-w-[360px] max-w-[500px] px-4 flex-col">
       <Outlet />
     </div>
   );
