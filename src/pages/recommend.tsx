89f93e5893750342496cfb42cd5c8441ec1d963f
jisooooooooooo <dlawltn0708@naver.com>
2025-01-12 05:21:58 +0900

    [feat] 댓글 api 로직 추가

--- src/pages/recommend.tsx
+++ src/pages/recommend.tsx
@@ -1,53 +1,60 @@
+import { useEffect } from "react";
 import Rollback from "@/assets/roll-back.svg?react";
 
 const Recommend = () => {
+  useEffect(() => {
+    // 스크롤 비활성화
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-    <main className="overflow-y-auto">
-      <h1 className="font-hsBombaram text-xl">오늘의 야식을 Pick! 해보세요</h1>
-      <div className="flex justify-between  mt-7">
-        <div>
-          <h2 className="text-lg font-semibold">오늘의 백색 요리</h2>
-          <h2 className="text-xl text-blue1 font-semibold">: 대파 방울토마토 구이</h2>
-        </div>
-        <button
-          type="button"
-          className="w-14 h-14 bg-blue1 rounded-full text-white font-semibold text-lg"
-        >
-          Pick!
-        </button>
-      </div>
-      <ul className="list-disc ml-5 flex flex-col gap-2 mt-7 mb-9">
-        <li className="text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-        <li className="text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-        <li className="text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-        <li className="text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-      </ul>
-      <div className="bg-gray-900 -mx-4 px-4 h-full min-h-[328px]">
-        <div className="flex justify-between  mt-7">
+    <main className="overflow-x-hidden flex flex-col">
+      <section className="space-y-7 flex flex-col pb-[35px] justify-center">
+        <div className="flex justify-between items-center">
           <div>
-            <h2 className="text-lg font-semibold text-white">오늘의 흑색 요리</h2>
-            <h2 className="text-xl text-yellow1 font-semibold">: 대파 방울토마토 구이</h2>
+            <p className="text-lg font-semibold">오늘의 백색 요리</p>
+            <p className="text-xl text-blue1 font-semibold">: 대파 방울토마토 구이</p>
           </div>
           <button
             type="button"
-            className="w-14 h-14 bg-yellow1 rounded-full text-black font-semibold text-lg"
+            className="w-14 h-14 bg-blue1 rounded-full text-white font-semibold text-lg"
           >
             Pick!
           </button>
         </div>
-        <ul className="list-disc ml-5 flex flex-col gap-2 mt-7 mb-9">
-          <li className="text-white text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-          <li className="text-white text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-          <li className="text-white text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
-          <li className="text-white text-lg">방울토마토의 라이코펜은 강력한 항산화제</li>
+        <ul className="list-disc ml-5 flex flex-col gap-2">
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
         </ul>
-        <div className="px-1">
-          <button className="h-11 w-full bg-white flex justify-center items-center gap-2 text-center rounded-lg">
-            <Rollback />
-            다시추천
+      </section>
+      <section className="-mx-4 px-4 space-y-7 bg-gray-900 text-white flex flex-col h-full pt-[38px]">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="text-lg font-semibold">오늘의 백색 요리</p>
+            <p className="text-xl text-yellow1 font-semibold">: 대파 방울토마토 구이</p>
+          </div>
+          <button
+            type="button"
+            className="w-14 h-14 bg-yellow1 rounded-full font-semibold text-lg text-black"
+          >
+            Pick!
           </button>
         </div>
-      </div>
+        <ul className="list-disc ml-5 flex flex-col gap-2">
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
+          <li className="text-md">방울토마토의 라이코펜은 강력한 항산화제</li>
+        </ul>
+        <button className="flex gap-2 text-md font-semibold py-2.5 px-4 bg-grey800 text-white w-max items-center rounded-lg mx-auto">
+          <Rollback color="white" />
+          다시 추천
+        </button>
+      </section>
     </main>
   );
 };
