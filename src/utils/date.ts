c5f7db08a0c5ff364d0dcd49af04fa98de5730b4
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-12 07:14:49 +0900

    feature: 기록 조회, 삭제 API 연동

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,6 +3,14 @@ export const formatDate = (date: Date | null) => {
   return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 };
 
+export const formatDashDate = (date: Date | null) => {
+  const year = String(date?.getFullYear()).padStart(4, "0");
+  const month = String((date?.getMonth() ?? 0) + 1).padStart(2, "0");
+  const day = String(date?.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
+
 export const formatTime = (date: Date | null) => {
   const year = String(date?.getFullYear()).padStart(4, "0");
   const month = String((date?.getMonth() ?? 0) + 1).padStart(2, "0");
