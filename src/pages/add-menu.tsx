7d72cbd56e097c1a8b68ec36a60ebffc1a2a1dac
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-12 06:10:28 +0900

    feature: 기록 추가하기 API 연동

--- src/pages/add-menu.tsx
+++ src/pages/add-menu.tsx
@@ -1,16 +1,17 @@
 import ArrowLeft from "@/assets/arrow-left.svg?react";
-import { FOOD_TYPE } from "@/constants/food-type";
+import { FOOD_TYPE, FoodType } from "@/constants/food-type";
 import { useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Photo from "@/assets/photo.svg?react";
 import PopupCalendar from "@/components/calendar/popup-calendar";
-import { formatDate } from "@/utils/date";
+import { formatDate, formatTime } from "@/utils/date";
 import useOutsideClick from "@/hooks/use-outside-click";
+import { createCalendar } from "@/services/calendar";
 
 interface Data {
   date: Date | null;
   category: string;
-  type: string;
+  type: FoodType;
   content: string;
   photo: string;
 }
@@ -31,6 +32,8 @@ function AddMenu() {
   const [data, setData] = useState<Data>(state || DEFAULT_VALUE);
   const [calendarOpen, setCalendarOpen] = useState(false);
   const [bottomDrawerOpen, setBottomDrawerOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [calendarRef] = useOutsideClick<HTMLDivElement>(() => setCalendarOpen(false));
   const [bottomDrawerRef] = useOutsideClick<HTMLDivElement>(() => setBottomDrawerOpen(false));
@@ -39,7 +42,8 @@ function AddMenu() {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const buttonDisabled = !data.date || !data.category || !data.type || !data.content;
+  const buttonDisabled =
+    !data.date || !data.category || !data.type || !data.content || !data.photo || isLoading;
 
   const bottomDrawerStyle = {
     transition: "height 0.3s",
@@ -57,7 +61,7 @@ function AddMenu() {
     });
   };
 
-  const onSelectType = (type: string) => {
+  const onSelectType = (type: FoodType) => {
     setData({
       ...data,
       type,
@@ -79,6 +83,7 @@ function AddMenu() {
   const onUploadPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
+    setSelectedFile(file);
 
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -108,9 +113,34 @@ function AddMenu() {
     setCalendarOpen(false);
   };
 
+  const onCreateCalendar = async () => {
+    setIsLoading(true);
+    try {
+      const formData = new FormData();
+      const formattedDate = formatTime(data.date);
+
+      formData.append("image", selectedFile as Blob);
+
+      const response = await createCalendar(
+        formData,
+        data.category,
+        data.type,
+        formattedDate,
+        data.content
+      );
+      if (response.status === 200) {
+        navigate("/history", { replace: true });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <main className="pt-1.5">
-      <header className="py-6 px-4 top-0 fixed bg-white z-10 w-full">
+      <header className="py-6 px-4 fixed top-0 bg-white z-10 w-full">
         <button onClick={onGoBack}>
           <ArrowLeft />
         </button>
@@ -206,12 +236,16 @@ function AddMenu() {
         className={`fixed bottom-0 w-[calc(100%-32px)] max-w-[468px] py-3 rounded-lg text-md text-white mb-9 ${
           buttonDisabled ? "bg-grey200" : "bg-grey900"
         }`}
+        onClick={onCreateCalendar}
       >
         저장
       </button>
-      {bottomDrawerOpen && <div className="fixed left-0 bg-black bg-opacity-50 h-full w-full" />}
+      {bottomDrawerOpen && (
+        <div className="fixed left-0 bg-black -top-[68px] bg-opacity-50 h-[calc(100%+68px)] w-full z-10" />
+      )}
+
       <div
-        className="bg-white w-full max-w-max-size bottom-0 fixed left-1/2 -translate-x-1/2 px-20 rounded-t-3xl space-y-7 overflow-hidden"
+        className="bg-white w-full max-w-max-size bottom-0 fixed left-1/2 -translate-x-1/2 px-20 rounded-t-3xl space-y-7 overflow-hidden z-50"
         style={bottomDrawerStyle}
         ref={bottomDrawerRef}
       >
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,3 +2,14 @@ export const formatDate = (date: Date | null) => {
   if (date === null) return "";
   return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 };
+
+export const formatTime = (date: Date | null) => {
+  const year = String(date?.getFullYear()).padStart(4, "0");
+  const month = String((date?.getMonth() ?? 0) + 1).padStart(2, "0");
+  const day = String(date?.getDate()).padStart(2, "0");
+  const hour = String(date?.getHours()).padStart(2, "0");
+  const minute = String(date?.getMinutes()).padStart(2, "0");
+  const second = String(date?.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+};
